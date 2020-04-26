d72bee898afc6fb78c542544b3cc0270e861f106
Tehuel <torresbaldi.tehuel@gmail.com>
2020-04-26 00:38:35 -0300

    adds image

--- components/hero.js
+++ components/hero.js
@@ -2,8 +2,9 @@ export default function HeroComponent () {
   return (
     <section className="hero is-medium is-light">
       <div className="hero-body">
-        <h1 className="title">Rick And Morty</h1>
-        <p className="subtitle has-text-grey">A Next.js framework exploration.</p>
+        <img src="/Rick_and_Morty.svg" alt="Rick And Morty"/>
+        <p className="subtitle has-text-dark">Rick And Morty Exploration.</p>
+        <p className="has-text-grey-dark">This project was made to learn Next.js framework.</p>
       </div>
     </section>
   )
